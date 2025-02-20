32149f130c5a5e673dc18789746b6bd7fd348e7e
Ten-T-Tonmoy <stu01754@gmail.com>
2025-02-21 04:41:15 +0600

    next and html+tailwind template added

---
+++ Html Tailwind JS/index.js
@@ -0,0 +1,9 @@
+const gbt = document.querySelector(".gbt");
+gbt.addEventListener("click", () => {
+  gbt.style.backgroundColor = "blue";
+});
+
+const bbt = document.querySelector(".bbt");
+bbt.addEventListener("click", () => {
+  bbt.style.backgroundColor = "aquamarine";
+});
