fbfd287a4534160d03f8a1014eac134ab727d274
Ten-T-Tonmoy <stu01754@gmail.com>
2025-02-21 04:29:14 +0600

    tailwind ts template added

---
+++ React+Tailwind_TypeScript/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div
+      className="h-screen bg-green-400 font-mono text-[30px] 
+      text-white flex items-center justify-center"
+    >
+      Tailwind+Typescript
+    </div>
+  );
+}
+
+export default App;
