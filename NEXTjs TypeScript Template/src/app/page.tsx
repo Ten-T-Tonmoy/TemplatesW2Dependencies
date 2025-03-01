a2395c67b42b8045271473aec3fbefdc63493fb6
Ten-T-Tonmoy <stu01754@gmail.com>
2025-03-01 21:34:26 +0600

    Next template added

---
+++ NEXTjs TypeScript Template/src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <>
+      <div>
+        <div
+          className="bg-green-500 m-10 text-center
+         rounded-md p-3 text-white text-2xl border-2 
+         hover:scale-110 cursor-pointer border-black"
+        >
+          NEXT JS Tailwind Template
+        </div>
+      </div>
+    </>
+  );
+}
